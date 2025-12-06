9e89548a10ef17659e6176a2b81c9e8817bd9366
Limuel <limuelmartincamangon@gmail.com>
2025-12-06 19:31:49 +0800

    init

--- Models/Items.cs
+++ Models/Items.cs
@@ -4,8 +4,8 @@ namespace CafeInventoryApi.Models
     public class Item
     {
         public int Id {get; set;}
-        public String Name {get; set;} = "";
-        public String Category {get; set;} = "";
+        public string Name {get; set;} = "";
+        public string Category {get; set;} = "";
         public int Quantity {get; set;}
         public decimal Price {get; set;}
         public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
